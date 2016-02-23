8489e2cb3a7656670685ee2779d864bea4bc4927
mgumiero9 <mgumiero9@gmail.com>
2016-02-23 00:25:48 -0300

    Using OkHttpClient to get weather information Giving manifest file Internet permission

--- app/src/main/java/com/androiders/stormy/MainActivity.java
+++ app/src/main/java/com/androiders/stormy/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import java.io.IOException;
 
 import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -32,14 +33,24 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
         Call call =  client.newCall(request);
-        try {
-            Response response = call.execute();
-            if (response.isSuccessful()) {
-                Log.v(TAG, response.body().string());
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
             }
-        } catch (IOException e) {
-            Log.e(TAG, "Exception caught: ", e);
-        }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    if (response.isSuccessful()) {
+                        Log.v(TAG, response.body().string());
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "Exception caught: ", e);
+                }
+            }
+        });
+
     }
 }
 ;

93266d9de034d269bffac6d0949e1b24060eae04
mgumiero9 <mgumiero9@gmail.com>
2016-02-23 16:09:31 -0300

    Implementing Error Handling

--- app/src/main/java/com/androiders/stormy/MainActivity.java
+++ app/src/main/java/com/androiders/stormy/MainActivity.java
@@ -22,8 +22,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String apiKey = "c0d6b355edd1c82fdc625e9b2392ee21";
-        double latitude = 37.8267;
+        double latitude =  9999; //37.8267;
         double longitude = -122.423;
+
         String forecastUrl = "https://api.forecast.io/forecast/" + apiKey + "/"
                 + latitude  + "," + longitude;
 
@@ -42,15 +43,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 try {
+                    Log.v(TAG, response.body().string());
                     if (response.isSuccessful()) {
-                        Log.v(TAG, response.body().string());
+                    } else {
+                        alertUserAboutError();
                     }
                 } catch (IOException e) {
                     Log.e(TAG, "Exception caught: ", e);
                 }
             }
         });
-
+        Log.d(TAG, "Main UI code is running");
+    }
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(), "error_dialog");
     }
 }
-;

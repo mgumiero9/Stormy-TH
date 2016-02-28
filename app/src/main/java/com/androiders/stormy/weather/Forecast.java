a7234729a152aacd3a6eeaec6062786a1c351b9a
mgumiero9 <mgumiero9@gmail.com>
2016-02-28 18:47:49 -0300

    Developing daily and hourly classes

--- app/src/main/java/com/androiders/stormy/weather/Forecast.java
+++ app/src/main/java/com/androiders/stormy/weather/Forecast.java
@@ -9,5 +9,27 @@ public class Forecast {
     private Hour[] mHourlyForecast;
     private Day[] mDaylyForecast;
 
+    public Current getCurrent() {
+        return mCurrent;
+    }
 
+    public void setCurrent(Current current) {
+        mCurrent = current;
+    }
+
+    public Hour[] getHourlyForecast() {
+        return mHourlyForecast;
+    }
+
+    public void setHourlyForecast(Hour[] hourlyForecast) {
+        mHourlyForecast = hourlyForecast;
+    }
+
+    public Day[] getDaylyForecast() {
+        return mDaylyForecast;
+    }
+
+    public void setDaylyForecast(Day[] daylyForecast) {
+        mDaylyForecast = daylyForecast;
+    }
 }

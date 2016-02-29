bfd477edc1e236a9cb91cba6773ee34a84a4046f
mgumiero9 <mgumiero9@gmail.com>
2016-02-29 01:59:02 -0300

    Adjusting coordinates to Faxinal do Soturno RS Brazil

--- app/src/main/java/com/androiders/stormy/ui/MainActivity.java
+++ app/src/main/java/com/androiders/stormy/ui/MainActivity.java
@@ -60,8 +60,8 @@ public class MainActivity extends AppCompatActivity {
 
         mProgressBar.setVisibility(View.INVISIBLE);
 
-        final double latitude = 37.8267;
-        final double longitude = -122.423;
+        final double latitude = -29.5762686;
+        final double longitude = -53.4504222;
 
         mRefreshImageView.setOnClickListener(new View.OnClickListener() {
             @Override

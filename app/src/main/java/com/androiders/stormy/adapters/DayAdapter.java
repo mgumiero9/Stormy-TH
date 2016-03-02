3efd35d6a3050d8512b02bf24d00785853496e9f
mgumiero9 <mgumiero9@gmail.com>
2016-03-02 00:16:07 -0300

    Creating DayAdapter class and extending BaseAdapter class - part2

--- app/src/main/java/com/androiders/stormy/adapters/DayAdapter.java
+++ app/src/main/java/com/androiders/stormy/adapters/DayAdapter.java
@@ -1,10 +1,14 @@
 package com.androiders.stormy.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.androiders.stormy.R;
 import com.androiders.stormy.weather.Day;
 
 /**
@@ -37,6 +41,49 @@ public class DayAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+
+        if (convertView == null) {
+            // brand new
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.daily_list_item, null);
+            holder = new ViewHolder();
+            holder.iconImageView = (ImageView) convertView.findViewById(R.id.iconImageView);
+            holder.dayLabel = (TextView) convertView.findViewById(R.id.dayNameLabel);
+            holder.temperatureLable = (TextView) convertView.findViewById(R.id.temperatureLabel);
+
+            convertView.setTag(holder);
+        }
+        else {
+
+        }
+
         return null;
     }
+
+    private static class ViewHolder {
+        ImageView iconImageView;
+        TextView temperatureLable;
+        TextView dayLabel;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

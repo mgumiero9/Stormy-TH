f3ccc51ac07856b2a89f6578de80a2aebf60cca5
mgumiero9 <mgumiero9@gmail.com>
2016-06-02 10:04:36 -0300

    Implementing Tapping through onClick method directly in HourAdapter class.

--- app/src/main/java/com/androiders/stormy/adapters/HourAdapter.java
+++ app/src/main/java/com/androiders/stormy/adapters/HourAdapter.java
@@ -1,11 +1,13 @@
 package com.androiders.stormy.adapters;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.androiders.stormy.MyUtils.FahrenheitToCelsius;
 import com.androiders.stormy.R;
@@ -16,11 +18,13 @@ import com.androiders.stormy.weather.Hour;
  */
 public class HourAdapter extends RecyclerView.Adapter<HourAdapter.HourViewHolder> {
 
+    private Context mContext;
     private Hour[] mHours;
 
-    public HourAdapter(Hour[] hours) {
-
+    public HourAdapter(Context context, Hour[] hours) {
+        mContext = context;
         mHours = hours;
+
     }
 
     @Override
@@ -41,7 +45,8 @@ public class HourAdapter extends RecyclerView.Adapter<HourAdapter.HourViewHolder
         return mHours.length;
     }
 
-    public class HourViewHolder extends RecyclerView.ViewHolder {
+    public class HourViewHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener {
 
         public TextView mTimeLabel;
         public TextView mSummaryLabel;
@@ -55,6 +60,8 @@ public class HourAdapter extends RecyclerView.Adapter<HourAdapter.HourViewHolder
             mSummaryLabel = (TextView) itemView.findViewById(R.id.summaryLabel);
             mTemperatureLabel = (TextView) itemView.findViewById(R.id.temperatureLabel);
             mIconImageView = (ImageView) itemView.findViewById(R.id.iconImageView);
+
+            itemView.setOnClickListener(this);
         }
 
         public void bindHour(Hour hour) {
@@ -63,6 +70,18 @@ public class HourAdapter extends RecyclerView.Adapter<HourAdapter.HourViewHolder
             mTemperatureLabel.setText(Math.round(FahrenheitToCelsius.fahrenheitToCelsius(hour.getTemperature())) + "");
             mIconImageView.setImageResource(hour.getIconId());
 
+        }
+
+        @Override
+        public void onClick(View v) {
+            String time = mTimeLabel.getText().toString();
+            String temperature = mTemperatureLabel.getText().toString();
+            String summary = mSummaryLabel.getText().toString();
+            String message = String.format("At %s it will be %s and %s",
+                    time,
+                    temperature,
+                    summary);
+            Toast.makeText(mContext, message, Toast.LENGTH_LONG).show();
         }
     }
 

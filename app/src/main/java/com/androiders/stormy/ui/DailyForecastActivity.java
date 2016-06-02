f7c736c06ff4d4a3a86b754278d457b341e2ea82
mgumiero9 <mgumiero9@gmail.com>
2016-06-01 23:33:41 -0300

    Implemented Toast message after tapping at Daily forecast item

--- app/src/main/java/com/androiders/stormy/ui/DailyForecastActivity.java
+++ app/src/main/java/com/androiders/stormy/ui/DailyForecastActivity.java
@@ -5,7 +5,10 @@ import android.content.Intent;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.androiders.stormy.R;
 import com.androiders.stormy.adapters.DayAdapter;
@@ -39,5 +42,21 @@ public class DailyForecastActivity extends ListActivity {
 
         DayAdapter adapter = new DayAdapter(this,  mDays);
         setListAdapter(adapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        String dayOftheWeek = mDays[position].getDayOfTheWeek();
+        String conditions = mDays[position].getSummary();
+        String highTemp = mDays[position].getTemperatureMax() + "";
+        String message = String.format("On %s the high will be %s and it will be %s",
+                dayOftheWeek,
+                highTemp,
+                conditions);
+
+        Toast.makeText(this, message, Toast.LENGTH_LONG)
+                .show();
     }
 }

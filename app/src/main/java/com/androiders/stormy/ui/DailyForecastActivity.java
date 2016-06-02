153ceccf14e5f9713ede1ebcb7b718c09e2a5a0d
mgumiero9 <mgumiero9@gmail.com>
2016-06-02 09:42:11 -0300

    Now Extending Activity instead of ListActivity for Daily forecast toast message.

--- app/src/main/java/com/androiders/stormy/ui/DailyForecastActivity.java
+++ app/src/main/java/com/androiders/stormy/ui/DailyForecastActivity.java
@@ -1,13 +1,16 @@
 package com.androiders.stormy.ui;
 
+import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.androiders.stormy.R;
@@ -17,14 +20,21 @@ import com.androiders.stormy.weather.Day;
 import java.lang.reflect.Array;
 import java.util.Arrays;
 
-public class DailyForecastActivity extends ListActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class DailyForecastActivity extends Activity {
 
     private Day[] mDays;
+
+    @Bind(android.R.id.list)    ListView mListView;
+    @Bind(android.R.id.empty)   TextView mEmptyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_forecast);
+        ButterKnife.bind(this);
 
         String[] daysOfTheWeek = { "Sunday", "Monday", "Tuesday", "Wednesday",
                                     "Thursday", "Friday", "Saturday" };
@@ -41,22 +51,23 @@ public class DailyForecastActivity extends ListActivity {
         mDays = Arrays.copyOf(parcelables, parcelables.length, Day[].class);
 
         DayAdapter adapter = new DayAdapter(this,  mDays);
-        setListAdapter(adapter);
+        mListView.setAdapter(adapter);
+        mListView.setEmptyView(mEmptyTextView);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String dayOftheWeek = mDays[position].getDayOfTheWeek();
+                String conditions = mDays[position].getSummary();
+                String highTemp = mDays[position].getTemperatureMax() + "";
+                String message = String.format("On %s the high will be %s and it will be %s",
+                        dayOftheWeek,
+                        highTemp,
+                        conditions);
+
+                Toast.makeText(DailyForecastActivity.this, message, Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
     }
 
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
-
-        String dayOftheWeek = mDays[position].getDayOfTheWeek();
-        String conditions = mDays[position].getSummary();
-        String highTemp = mDays[position].getTemperatureMax() + "";
-        String message = String.format("On %s the high will be %s and it will be %s",
-                dayOftheWeek,
-                highTemp,
-                conditions);
-
-        Toast.makeText(this, message, Toast.LENGTH_LONG)
-                .show();
-    }
 }
